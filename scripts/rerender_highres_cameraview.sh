be227cecb8e98016ac5bd5ee5ecc5094c1e5afc5
Tsung-Wei Ke <42392466+twke18@users.noreply.github.com>
2024-04-03 23:56:00 -0400

    Update rerender_highres_cameraview.sh

--- scripts/rerender_highres_cameraview.sh
+++ scripts/rerender_highres_cameraview.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 SPLIT=train
-DEMO_ROOT=./data/raw/${SPLIT}
-RAW_SAVE_PATH=./data/raw_highres/${SPLIT}
-PACKAGE_SAVE_PATH=./data/packaged_highres/${SPLIT}
+DEMO_ROOT=./data/peract/raw/${SPLIT}
+RAW_SAVE_PATH=./data/peract/raw_highres/${SPLIT}
+PACKAGE_SAVE_PATH=./data/peract/packaged_highres/${SPLIT}
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
+# Re-render high-resolution camera views
 for task in place_cups close_jar insert_onto_square_peg light_bulb_in meat_off_grill open_drawer place_shape_in_shape_sorter place_wine_at_rack_location push_buttons put_groceries_in_cupboard put_item_in_drawer put_money_in_safe reach_and_drag slide_block_to_color_target stack_blocks stack_cups sweep_to_dustpan_of_size turn_tap
 do
     python data_preprocessing/rerender_highres_rlbench.py \
@@ -20,6 +21,10 @@ do
 
 done
 
+# Re-arrange directory
+python data_preprocessing/rearrange_rlbench_demos.py --root_dir $(pwd)/data/peract/raw_highres/${SPLIT}
+
+# Package episodes into .dat fiels
 for task in place_cups close_jar insert_onto_square_peg light_bulb_in meat_off_grill open_drawer place_shape_in_shape_sorter place_wine_at_rack_location push_buttons put_groceries_in_cupboard put_item_in_drawer put_money_in_safe reach_and_drag slide_block_to_color_target stack_blocks stack_cups sweep_to_dustpan_of_size turn_tap
 do
     python data_preprocessing/package_rlbench.py \
