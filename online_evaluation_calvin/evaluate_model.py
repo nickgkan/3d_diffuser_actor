92e0f30aefd8d110012270fb25672ceafce5634c
twke18 <tsungwek@andrew.cmu.edu>
2024-08-16 11:11:15 -0400

    update new model on calvin

--- online_evaluation_calvin/evaluate_model.py
+++ online_evaluation_calvin/evaluate_model.py
@@ -168,7 +168,6 @@ class DiffusionModel(CalvinBaseModel):
 
         # history of actions
         gripper = torch.as_tensor(obs["proprio"]).to(device).unsqueeze(0)
-        gripper = gripper[:, -self.args.num_history:]
 
         trajectory = self.policy(
             fake_trajectory.float(),
